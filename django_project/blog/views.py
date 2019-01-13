892dddcf939ddc1a132fbf44a14f8f682b610d04
Your Name <you@example.com>
2019-01-12 21:11:44 -0800

    got up to 20:00 on chapter 3

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):#this function returns a line of html code for a browser
-    return HttpResponse('<h1>Blog Home</h1>') #html for a browser to read
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
 
-# Create your views here.
+def home(request):
+    context = {
+            'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
