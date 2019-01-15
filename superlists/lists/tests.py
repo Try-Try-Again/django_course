2a50b3455d6a0375da88d524b5fdf8fafc2a661a
Your Name <you@example.com>
2019-01-15 09:49:01 -0800

    Refactor home page view to use a template

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -4,6 +4,7 @@ from django.urls import resolve
 from django.test import TestCase
 #import the django HttpRequest
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 #imports the views.py function that we're gonna write later
 from lists.views import home_page
@@ -11,27 +12,6 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
-        # check the root of the site and find the funciton
-        found = resolve('/')
-        # check if found.func (resolve.func) == what out home_page function
-        # will return
-        self.assertEqual(found.func, home_page)
-
-
-    def test_home_page_returns_correct_html(self):
-        #create an HttpRequest object
-        request = HttpRequest()
-        #call our home_page question with request as our arguement
-        #store what it returns in 'response'
-        response = home_page(request)
-        #since the content of the response is raw bytes, we need to decode
-        #into utf8
-        #assign the decoded response to 'html'
-        html = response.content.decode('utf8')
-        #make sure that 'html' has an html tag up front
-        self.assertTrue(html.startswith('<html>'))
-        #make sure that it has a title called To-Do lists
-        self.assertIn('<title>To-Do lists</title>', html)
-        #make sure that it ends with </html>
-        self.assertTrue(html.endswith('</html>'))
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
