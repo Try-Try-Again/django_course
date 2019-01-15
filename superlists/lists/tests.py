55050e9ca9a51090b7b18df2ed0e09f82dbd986c
Your Name <you@example.com>
2019-01-14 16:15:20 -0800

    First unit test and url mapping, dummy view

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,6 +1,15 @@
+#resolves URLs and finds out what view function they should be mapped to
+from django.urls import resolve
+#imports a special version of unittest's TestCase
 from django.test import TestCase
+#imports the views.py function that we're gonna write later
+from lists.views import home_page
 
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
 
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+    def test_root_url_resolves_to_home_page_view(self):
+        # check the root of the site and find the funciton
+        found = resolve('/')
+        # check if found.func (resolve.func) == what out home_page function
+        # will return
+        self.assertEqual(found.func, home_page)
