5ba4f37af146267856ebbed7fd6792f9153e5569
Your Name <you@example.com>
2019-01-15 10:13:03 -0800

    Front page HTML now generated from a template

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -52,13 +52,14 @@ class NewVisitorTest(unittest.TestCase):
 
         # When she hits enter, the page updates, and the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
-        inputbox.send_keys(Keys.Enter)
+        inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New todo item did not appear in table"
         )
 
         # There is still a text box inviting her to add another item. She
