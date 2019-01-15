bf7a3a264221cbc7656f9e11d22a6a29aeda11b4
Your Name <you@example.com>
2019-01-14 17:26:31 -0800

    Basic view now returns minimal html

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,6 +1,42 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+#tests are arranged in classes which inherit from unittest.Testcase
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    #starts before each test
+    def setUp(self):
+        #creates a selenium webbrowser object
+        self.browser = webdriver.Firefox()
+    #starts after each test
+    def tearDown(self):
+        #close browser object
+        self.browser.quit()
+    #them main function of our test so far
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        # open project homepage in selenium
+        self.browser.get('http://localhost:8000')
+        # She notices the page title and header mention to-do lists
+        # check to see if 'To-Do' is inside the browser's title attribute
+        self.assertIn('To-Do', self.browser.title)
+        #self.fail fails no matter what. this just returns a message
+        self.fail('Finish the test!')
+        # She is invited to enter a to-do item straight away
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is trying fly-fishing lures)
+        # When she hits enter, the page updates, and the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to makes a fly" (Edith is very methodical)
+        # The page updates again, and not shows both items on her list
+        # Edith wonders whether the site will remember her list. Then she sees
+        # that the site has generated a unique URL for her == there is some
+        # explanatory text to that effect.
+        # She visits that URL - her to-do list is still there.
+        # Satisfied, she back to sleep
+#onlu run if ran on its own
+if __name__ == '__main__':
+    #run our tests
+    unittest.main(warnings='ignore')
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -2,8 +2,12 @@
 from django.urls import resolve
 #imports a special version of unittest's TestCase
 from django.test import TestCase
+#import the django HttpRequest
+from django.http import HttpRequest
+
 #imports the views.py function that we're gonna write later
 from lists.views import home_page
+
 
 class HomePageTest(TestCase):
 
@@ -13,3 +17,21 @@ class HomePageTest(TestCase):
         # check if found.func (resolve.func) == what out home_page function
         # will return
         self.assertEqual(found.func, home_page)
+
+
+    def test_home_page_returns_correct_html(self):
+        #create an HttpRequest object
+        request = HttpRequest()
+        #call our home_page question with request as our arguement
+        #store what it returns in 'response'
+        response = home_page(request)
+        #since the content of the response is raw bytes, we need to decode
+        #into utf8
+        #assign the decoded response to 'html'
+        html = response.content.decode('utf8')
+        #make sure that 'html' has an html tag up front
+        self.assertTrue(html.startswith('<html>'))
+        #make sure that it has a title called To-Do lists
+        self.assertIn('<title>To-Do lists</title>', html)
+        #make sure that it ends with </html>
+        self.assertTrue(html.endswith('</html>'))
