a99bc27b6e738230bbc0b6aac6f1cbe01ad0c08c
Your Name <you@example.com>
2019-01-16 09:22:07 -0800

    refactored our functional test

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -20,6 +20,14 @@ class NewVisitorTest(unittest.TestCase):
         #close browser object
         self.browser.quit()
 
+    #we ran somthing similar to this 3 times in a row.
+    #whenever we run something 3 times in 1 script, refactor it into a funciton
+    def check_for_row_in_list_table(self, row_text):
+        #assign the element with id 'id_list_table'
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     #them main function of our test so far
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
@@ -54,6 +62,7 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to makes a fly" (Edith is very methodical)
@@ -63,13 +72,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # The page updates again, and not shows both items on her list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn(
-                '2: Use peacock feathers to make a fly',
-                 [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her == there is some
